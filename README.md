3685ff8506f33f170707e0ff408fb75386015f5a
jmanek <jesse.manek@gmail.com>
2021-09-13 00:10:08 -0700

    Move third party

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ pip install -r requirements.txt
 python index.py 0 0 https://google.com https://wikipedia.com
 ```
 This will create two labels starting at the label in position (0, 0)
-![QR label](qr_label.png)
+![QR label](templates/qr_label.png)
--- index.py
+++ index.py
@@ -3,13 +3,16 @@ from pdf2image import convert_from_path, convert_from_bytes
 from PIL import Image, ImageFont, ImageDraw
 import argparse
 from functools import reduce
+import math
+import numpy as np
+import cv2 as cv
+import platform
 
 
 def make_qr_code(data):
     qr = qrcode.QRCode(
         version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=50,
+        box_size=20,
         border=2,
     )
     qr.add_data(data)
@@ -18,30 +21,52 @@ def make_qr_code(data):
     return (img, qr)
 
 
-# for garagesalepup 1" square label sheet
-# top 100px
-# left 100px x 2 = 200px
-# box 200px x 7 = 1400px
-# margin ~17px x 6 ~= 100px
-def overlay_label(qr_image, label, template, col, row):
-    qr_image = qr_image.resize((195, 195))
-    col_offset = col * 217 + 100
-    row_offset = row * 225 + 100
-    template.paste(qr_image, (col_offset, row_offset))
+def overlay_label(qr_image, qr, label, template, rect):
+    width, height = template.size
+    print(rect)
+    qr_image = qr_image.resize((rect[2]-10, rect[3]-10))
 
-    draw = ImageDraw.Draw(template)
-
-    font = ImageFont.truetype("arial.ttf", 10)
-    draw.text((col_offset, row_offset-20),
-              label[:40], font=font, fill=(0, 0, 0))
+    template.paste(qr_image, (rect[0] + 5, rect[1] + 5))
+    # Font size based on template dimensions
+    font = ImageFont.truetype("arial.ttf", 42)
+    ImageDraw.Draw(template).text((rect[0], rect[1]-20),
+                                  label[:40], font=font, fill=(0, 0, 0))
+    print_qr_code(qr)
 
 
-def create_template():
-    template = Image.new('RGB', (1700, 2200), 'white')
-    return template
+def create_template(template_path):
+    template = Image.new('RGB', (8500, 11000), 'white')
+    if template_path.endswith('pdf'):
+        if platform.system() == 'Windows':
+            img = convert_from_path(
+                template_path, poppler_path='./external/poppler-win64-21.09.0/Library/bin')[0]
+        else:
+            img = convert_from_path(template_path)
+    else:
+        img = Image.open(template_path)
+    t_width, t_height = template.size
+    width, height = img.size
+    img = cv.cvtColor(np.array(img), cv.COLOR_RGB2BGR)
+
+    imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    ret, thresh = cv.threshold(imgray, 127, 255, 0)
+    contours, hierarchy = cv.findContours(
+        thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    if len(list(filter(lambda cc: cc[3] == -1, hierarchy[0]))) == 1:
+        contour_level = 0
+    else:
+        contour_level = -1
+    rects = list(map(lambda cc: (t_width*cc[0]//width, t_height*cc[1]//height, t_width*cc[2]//width, t_height*cc[3]//height),
+                     map(lambda cc: cv.boundingRect(cc[1]),
+                     filter(lambda cc: cc[0][3] == contour_level,
+                            zip(hierarchy[0], contours)))))
+    rects.reverse()
+    return template, rects
 
 
-# github.com/alishtory/qrcode-terminal
+# adapated from github.com/alishtory/qrcode-terminal
 def print_qr_code(qr):
     white_block = '\033[0;37;47m  '
     black_block = '\033[0;37;40m  '
@@ -59,31 +84,26 @@ def print_qr_code(qr):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Create printable QR code label')
-    parser.add_argument('row', type=int,
-                        help='The row to start adding labels -- zero indexed')
-    parser.add_argument('col', type=int,
-                        help='The column to start adding labels -- zero indexed')
+    parser.add_argument('template', type=str,
+                        help='The template to use')
+    parser.add_argument('start', type=int,
+                        help='The square to start adding labels -- zero indexed')
     parser.add_argument('data', nargs='+',
                         help='space separate list of links to convert into QR codes')
-
     args = parser.parse_args()
-    row = args.row
-    col = args.col
 
-    template = create_template()
-    # template = Image.open("template.png")
+    start = args.start
+    template, rects = create_template(args.template)
+    if start + len(args.data) > len(rects):
+        raise Exception('Not enough space left in template')
+    rects = rects[start: start + len(rects)]
+    rects.reverse()
 
-    for datum in args.data:
-        if col == 7:
-            if row >= 8:
-                raise Exception(
-                    'Reached end of template, no free spaces remain')
-            else:
-                col = 0
-                row += 1
-        qr_img, qr = make_qr_code(datum)
-        print(datum)
-        print_qr_code(qr)
-        overlay_label(qr_img, datum, template, col, row)
-        col += 1
-    template.save('qr_label.pdf')
+    list(map(lambda qr: overlay_label(*qr[0], qr[1], template, rects.pop()),
+             map(lambda dd: (make_qr_code(dd), dd), args.data)))
+
+    print('Saved to qr_label.pdf/png')
+    # Resolution based on template dimensions
+    template.save('qr_label.pdf', resolution=1000)
+    template.save('qr_label.png')
+    template.show()
